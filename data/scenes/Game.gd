fcdcf7c71968607b36f852bf52c1a25b8ad9aaa6
Artur Yamada <arturyamada@alunos.utfpr.edu.br>
2022-02-08 00:32:05 -0300

    Added Fade in and out
    Added fade in when booting the game
    
    Added fade out and fade in when changing stages

--- data/scenes/Game.gd
+++ data/scenes/Game.gd
@@ -8,11 +8,15 @@ var current_stage = 0
 var house_holder = TYPES.HOMELESS
 var map_offset = Vector2(39,7)
 
+onready var anim_player = get_node("CanvasModulate/AnimationPlayer")
+
 # Scenes
 var stage_1_scene = preload("res://stage1.tscn")
 var stage_2_scene = preload("res://stage2.tscn")
 
 func _ready() -> void:
+	anim_player.play_backwards("Fade")
+	yield(anim_player, "animation_finished")
 	pass
 	#AudioManager.play_music(music)
 	#$UI/AnimationPlayer.play("New Anim")
@@ -20,9 +24,14 @@ func _ready() -> void:
 func _on_Map_house_data_request():
 	var map = self.get_child(self.get_child_count()-1)
 	
+	
 	map.spawn_house(house_holder)
 
 func _on_House_upgrade_house(new_house):
+	
+	anim_player.play("Fade")
+	yield(anim_player, "animation_finished")
+	
 	var map = self.get_child(self.get_child_count()-1)
 	map.queue_free()
 	
@@ -33,12 +42,20 @@ func _on_House_upgrade_house(new_house):
 			var stage_1 = stage_1_scene.instance()
 			stage_1.position = map_offset
 			self.add_child(stage_1)
+			
+			anim_player.play_backwards("Fade")
+			yield(anim_player, "animation_finished")
+			
 		1:
 			house_holder = new_house
 			current_stage += 1
 			var stage_2 = stage_2_scene.instance()
 			stage_2.position = map_offset
 			self.add_child(stage_2)
+			
+			anim_player.play_backwards("Fade")
+			yield(anim_player, "animation_finished")
+			
 		2:
 			print("INSERT ENDING HERE BEEP BOOP")
 			get_tree().quit()
